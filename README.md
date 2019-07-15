3659d4aa934cd4e92b27ac0539638150d0d5aefe
chevallier <chevallier.th@gmail.com>
2019-07-15 17:02:42 +0900

    add python code + edit README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # langton_ant
 Python/TkInter graphical implementation of Langton's ant
+
+Langton's ant walks on a 2D grid with the following rules :
+  - If it is in a white cell, it turns right and the cell it was on becomes black
+  - if it was on a black cell, it turns left and the cell it was on becomes white
+
+This simple program offers a TkInter GUI to visualize the trajectory of the ant.
+We can observe that :
+  - for the first ~ 100 steps, it follows a pretty symetrical pattern
+  - for the next ~ 10k moves, the trajectory becomes chaotic
+  - then it enters a cyclic pattern moving endlessly, called the "highway"
---
+++ langton_ant.py
@@ -0,0 +1,229 @@
+from tkinter import *
+import math
+
+# colors
+WHITE = "white"
+BLACK = "black"
+RED = "red"
+BLUE = "lightblue"
+
+# directions
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+# TK config
+X_CELLS = 91
+Y_CELLS = 61
+CELL_SIZE = 12
+SLEEP_TIME = 1  # time in ms between 2 moves
+
+
+class Cell:
+    def __init__(self, widget, x, y, color=WHITE):
+        self.widget = widget
+        self.x = x
+        self.y = y
+        self.color = color
+
+
+class Ant:
+    def __init__(self, widget, x, y, direction=UP):
+        self.widget = widget
+        self.x = x
+        self.y = y
+        self.dir = direction
+
+
+class App(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+        self.cells = dict()
+        self.finished = False   # cant go any further due to grid size
+        self.stopped = False    # user interrupted the run
+        self.running = False    # currently running (avoid multiple callbacks)
+        self.steps = 0
+
+        if X_CELLS % 2 == 0 or Y_CELLS % 2 == 0:
+            raise Exception("The number of cells in both directions must be odd.")
+
+        # Title
+        self.title_label = Label(self, text="Langton's ant", justify=CENTER)
+        self.title_label.pack()
+
+        # Frame
+        self.frame = Frame(self, padx=1, pady=1, borderwidth=2, relief=GROOVE)
+        self.frame.pack()
+
+        # Canvas Grid and squares
+        self.canvas = Canvas(self.frame,
+                             width=10 + X_CELLS * CELL_SIZE,
+                             height=10 + Y_CELLS * CELL_SIZE,
+                             bd=0, background=WHITE)
+        for i in range(0, 1 + X_CELLS):
+            self.canvas.create_line(5 + CELL_SIZE * i, 5,
+                                    5 + CELL_SIZE * i, 5 + Y_CELLS * CELL_SIZE,
+                                    fill=BLACK)
+        for j in range(0, 1 + Y_CELLS):
+            self.canvas.create_line(5, 5 + CELL_SIZE * j,
+                                    5 + X_CELLS * CELL_SIZE, 5 + CELL_SIZE * j,
+                                    fill=BLACK)
+        self.canvas.pack()
+
+        for i in range(0, X_CELLS):
+            for j in range(0, Y_CELLS):
+                cell = self.canvas.create_rectangle(
+                    5 + i * CELL_SIZE + 1, 5 + j * CELL_SIZE + 1,
+                    (i+1) * CELL_SIZE + 5, 5 + (j+1) * CELL_SIZE,
+                    fill=BLUE, outline="")
+                self.cells[(i, j)] = Cell(cell, i, j)
+
+        # Ant
+        self.x0 = math.trunc(X_CELLS / 2)
+        self.y0 = math.trunc(Y_CELLS / 2)
+        ant_widget = self.canvas.create_oval(
+            5 + self.x0 * CELL_SIZE + 1 + 1,    5 + self.y0 * CELL_SIZE + 1 + 1,
+            5 + (self.x0 + 1) * CELL_SIZE - 1,  5 + (self.y0 + 1) * CELL_SIZE - 1,
+            fill=RED, outline="")
+        self.ant = Ant(ant_widget, self.x0, self.y0)
+
+        # Start / Stop / Reset buttons
+
+        self.bottom_frame = Frame(self)
+        self.bottom_frame.pack()
+
+        self.next_button = Button(self, text="Next", command=self.next_button_click)
+        self.next_button.pack(side="left")
+        self.run_button = Button(self, text="Run", command=self.run_button_click)
+        self.run_button.pack(side="left")
+        self.stop_button = Button(self, text="Stop", command=self.stop_button_click)
+        self.stop_button.pack(side="left")
+        self.reset_button = Button(self, text="Reset", command=self.reset_button_click)
+        self.reset_button.pack(side="left")
+
+        # steps count
+        self.steps_label1 = Label(self, text="Step : ")
+        self.steps_label2 = Label(self, text="0")
+        self.steps_label2.pack(side="right")
+        self.steps_label1.pack(side="right")
+
+    def get_next_cell(self, direction):
+        x = self.ant.x
+        y = self.ant.y
+        if direction == UP:
+            return x, y - 1
+        if direction == RIGHT:
+            return x + 1, y
+        if direction == DOWN:
+            return x, y + 1
+        if direction == LEFT:
+            return x - 1, y
+
+    def ant_cell_color(self):
+        return self.cells[(self.ant.x, self.ant.y)].color
+
+    def get_next_dir(self):
+        curr_color = self.ant_cell_color()
+        if curr_color == BLACK:
+            return left_dir(self.ant.dir)
+        elif curr_color == WHITE or curr_color == BLUE:
+            return right_dir(self.ant.dir)
+
+    def move_ant(self):
+        direction = self.get_next_dir()
+        (next_x, next_y) = self.get_next_cell(direction)
+        if not 0 <= next_x < X_CELLS or not 0 <= next_y < Y_CELLS:
+            return False    # out of grid
+        else:
+            dx = (next_x - self.ant.x) * CELL_SIZE
+            dy = (next_y - self.ant.y) * CELL_SIZE
+            self.canvas.move(self.ant.widget, dx, dy)
+            self.steps += 1
+            self.steps_label2.config(text=str(self.steps))
+            self.ant.x = next_x
+            self.ant.y = next_y
+            self.ant.dir = direction
+            return True
+
+    def set_cell_color(self, x, y, color):
+        cell = self.cells[(x, y)]
+        cell.color = color           # logical flip
+        self.canvas.itemconfigure(cell.widget, fill=color)  # physical flip
+
+    def flip_cell_color(self, x, y):
+        cell = self.cells[(x, y)]
+        next_color = get_flipped_color(cell.color)
+        self.set_cell_color(x, y, next_color)
+
+    def next_turn(self):
+        cell_before_move = self.cells[(self.ant.x, self.ant.y)]
+        moved = self.move_ant()
+        if moved:
+            self.flip_cell_color(cell_before_move.x, cell_before_move.y)
+        return moved
+
+    def start_play_loop(self):
+        if self.finished or self.stopped:
+            return
+        moved = self.next_turn()
+        if moved:
+            self.after(SLEEP_TIME, self.start_play_loop)
+        else:
+            self.finished = True
+
+    def next_button_click(self):
+        if self.finished or self.running:
+            return
+        moved = self.next_turn()
+        if not moved:
+            self.finished = True
+
+    def run_button_click(self):
+        if self.finished or self.running:
+            return
+        self.stopped = False
+        self.running = True
+        self.start_play_loop()
+        self.running = False
+
+    def stop_button_click(self):
+        self.stopped = True
+
+    def reset_button_click(self):
+        # reset grid
+        for i in range(0, X_CELLS):
+            for j in range(0, Y_CELLS):
+                self.set_cell_color(i, j, BLUE)
+        # reset ant
+        dx = (self.x0 - self.ant.x) * CELL_SIZE
+        dy = (self.y0 - self.ant.y) * CELL_SIZE
+        self.canvas.move(self.ant.widget, dx, dy)
+        self.steps = 0
+        self.ant.x = self.x0
+        self.ant.y = self.y0
+        self.ant.dir = UP
+        #reset flags
+        self.stopped = False
+        self.finished = False
+        self.running = False
+
+
+def right_dir(direction):
+    return (direction + 1) % 4    # since directions are numbered from 0 to 3
+
+
+def left_dir(direction):
+    return (direction - 1) % 4    # since directions are numbered from 0 to 3
+
+
+def get_flipped_color(color):
+    if color == BLACK:
+        return WHITE
+    if color == WHITE or color == BLUE:
+        return BLACK
+    raise Exception("Unknown color : " + color)
+
+
+app = App()
+app.mainloop()
